eea7525a42609ad3c16a3a182aa9ca71e91af452
Munohane <bsingapore69@gmail.com>
2025-12-05 22:08:20 +0100

    Menu + Titre et contenu de la page (Carte de redirection) + Style

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-const navbarToggler = document.getElementById('navbar-toggler');
-const navbarCollapse = document.getElementById('navbar-collapse');
-
-navbarToggler.addEventListener('click', () => {
-  navbarCollapse.classList.toggle('show');
-});
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'bastiaan-black': '#0a0a0a',
+                'bastiaan-blue': '#1e3a8a', // Bleu roi foncé
+                'bastiaan-gold': '#d4af37', // Doré classique
+                'bastiaan-gold-light': '#f3e5ab',
+            },
+            fontFamily: {
+                'heading': ['"Playfair Display"', 'serif'],
+                'body': ['"Lato"', 'sans-serif'],
+            }
+        }
+    }
+}
